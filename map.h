6a0b528d2ffee3c535b2917b4f4c5108b27baf81
TomShaoquan <51372791+TomShaoquan@users.noreply.github.com>
2019-12-17 13:48:10 +0800

    图
    map

---
+++ map.h
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+#define MaxSize 1024
+
+//要保存的基本数据
+typedef struct EdgeNode	//边的节点
+{
+	int adjvex;	       //邻接顶点编号
+	int weight;
+	struct EdgeNode* next;//下一条边
+}EdgeNode;
+
+typedef struct _VertexNode{	//顶点节点
+	char data;			//顶点数据 
+	struct EdgeNode* first;	//指向邻接第一条边  头节点
+}VertexNode,AdjList;
+
+typedef struct _AdjListGraph{
+	AdjList* adjlist;
+	int vex;	//顶点数
+	int edge;	//边数
+}AdjListGraph;
